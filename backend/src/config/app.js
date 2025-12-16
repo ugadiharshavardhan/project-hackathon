da4bf03c4e9e08e96e45288098b027552de7bd59
harsha7075226822 <ugadiharshavardhan@gmail.com>
2025-12-16 16:23:39 +0530

    v4.3 completed

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://project-hackathon-theta.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
