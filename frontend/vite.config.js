33d0371ed84f6bc1e69068fd1a063711064baa82
harsha7075226822 <ugadiharshavardhan@gmail.com>
2025-12-16 18:42:07 +0530

    Fix Vercel SPA routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react(), tailwindcss()],
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//   },
+//   server: {
+//     historyApiFallback: true,
+//   },
+// })
+
+
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  server: {
-    historyApiFallback: true,
-  },
+  }
 })
